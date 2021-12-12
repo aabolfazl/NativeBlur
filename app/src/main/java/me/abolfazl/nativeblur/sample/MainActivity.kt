a910831a957f4c7e80f3ba3584553ef7ee07e8b5
abbasiabolfazl <aabolfazlit@gmail.com>
2021-12-12 14:55:26 +0300

    add OverlayBlurView.kt

--- app/src/main/java/me/abolfazl/nativeblur/sample/MainActivity.kt
+++ app/src/main/java/me/abolfazl/nativeblur/sample/MainActivity.kt
@@ -2,13 +2,19 @@ package me.abolfazl.nativeblur.sample
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import me.abolfazl.nativeblur.OverlayBlurView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var overlayBlurView: OverlayBlurView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        overlayBlurView = OverlayBlurView(this)
 
         seekbarActivityButton.setOnClickListener {
             startActivity(Intent(this, SeekbarActivity::class.java))
@@ -18,5 +24,30 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, StaticActivity::class.java))
         }
 
+        blurActivity.setOnClickListener {
+            overlayBlurView.show()
+            AlertDialog.Builder(this)
+                .setCancelable(false)
+                .setTitle("This dialog just for test!")
+                .setMessage("The background blurred :)")
+                .setPositiveButton("Just close dialog") { dialog, _ -> dialog.dismiss() }
+                .setNeutralButton("Close and Hide Blur") { _, _ -> overlayBlurView.hide() }
+                .show()
+        }
+
+        blurActivity.setOnLongClickListener {
+            overlayBlurView.reset()
+            true
+        }
+
+        overlayBlurView.clickListener = View.OnClickListener { overlayBlurView.hide() }
+    }
+
+    override fun onBackPressed() {
+        if (overlayBlurView.isShowing) {
+            overlayBlurView.hide()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
---
+++ native-blur/src/main/java/me/abolfazl/nativeblur/OverlayBlurView.kt
@@ -0,0 +1,117 @@
+package me.abolfazl.nativeblur
+
+import android.app.Activity
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.ViewGroup
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import java.lang.ref.WeakReference
+
+class OverlayBlurView(parentActivity: Activity?, show: Boolean = false) {
+    private lateinit var parentObject: WeakReference<Activity>
+    private var blurredView: View? = null
+    var clickListener: OnClickListener? = null
+
+    var isShowing: Boolean = false
+        private set
+
+    init {
+        parentActivity?.let {
+            if (show) {
+                checkParentActivity(parentActivity)
+            } else {
+                parentObject = WeakReference(parentActivity)
+            }
+        }
+    }
+
+    fun show() {
+        if (isShowing) {
+            return
+        }
+
+        if (blurredView != null) {
+            blurredView?.visibility = View.VISIBLE
+            isShowing = true
+        } else {
+            val parent = parentObject.get()
+
+            if (parent is Activity) {
+                getActivityRootView(parent)?.let {
+                    prepareView(it)
+                }
+            }
+        }
+    }
+
+    private fun getActivityRootView(activity: Activity?): View? {
+        return activity?.window?.decorView?.findViewById(android.R.id.content)
+    }
+
+    private fun checkParentActivity(parentActivity: Activity) {
+        getActivityRootView(parentActivity)?.let { view ->
+            view.viewTreeObserver
+                .addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
+                    override fun onGlobalLayout() {
+                        view.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                        prepareView(view)
+                    }
+                })
+        }
+    }
+
+    private fun prepareView(rootView: View) {
+        if (isShowing || blurredView != null) {
+            return
+        }
+
+        if (rootView is ViewGroup) {
+            isShowing = true
+            val width: Int = rootView.width
+            val height: Int = rootView.height
+
+            val w = (width / 5.0f).toInt()
+            val h = (height / 5.0f).toInt()
+            val bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bitmap)
+            canvas.scale(1.0f / 5.0f, 1.0f / 5.0f)
+            rootView.draw(canvas)
+            NativeBlur.blurBitmap(
+                bitmap, 8.coerceAtLeast(w.coerceAtLeast(h) / 180)
+            )
+
+            blurredView = View(rootView.context).also {
+                it.background = BitmapDrawable(bitmap)
+            }
+            clickListener?.let {
+                blurredView?.setOnClickListener(it)
+            }
+
+            rootView.removeView(blurredView)
+            rootView.addView(
+                blurredView,
+                ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            )
+        } else {
+            Logger.error("Root view should be a ViewGroup class.")
+        }
+    }
+
+    fun hide() {
+        if (isShowing) {
+            isShowing = false
+            blurredView?.visibility = View.GONE
+        }
+    }
+
+    fun reset() {
+        isShowing = false
+        blurredView = null
+    }
+}
